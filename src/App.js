a9a4d5b1da120921f3d8a4d01c3da6a2e3fccd38
José Etchichury <jcetchichury@hotmail.com>
2022-08-08 14:19:51 -0300

    refactor: Alteração do App.js retirando o pré-instalado e adicionando o compoment listaPokemon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import ListaPokemon from './Components/listaPekemons/listaPokemon.jsx';
 import './App.css';
 
 function App() {
   return (
     <>
       <h1> Partiu Criar "O Pokedex" em React.JS</h1>
+      <ListaPokemon />
     </>
   );
 }
