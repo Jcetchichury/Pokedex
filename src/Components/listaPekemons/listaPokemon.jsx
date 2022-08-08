dea1fc517c89df28690adc3d8ba694039a3ef22b
José Etchichury <jcetchichury@hotmail.com>
2022-08-08 14:18:30 -0300

    feat: Adiciona component listaPokemon.
    feat: adiciona o Style do componet listaPokemon

---
+++ src/Components/listaPekemons/listaPokemon.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./listaPokemons.css"
+
+export default function ListaPokemon (){
+    return(
+        <>
+            <div className="onOff"></div>
+            <div className="on"></div>
+            <div className="off"></div>
+            <div className="borda2">
+                <div className="borda"> 
+                    <div className="lista">
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
