fec2f244d7399cb5a3719e8b4a38f2d7dda1df79
José Etchichury <jcetchichury@hotmail.com>
2022-08-11 01:38:42 -0300

    refactory: Adiocionado o pokeApi no projeto Integração do api com o botão do pokemons

--- src/Hooks/DataFetch.js
+++ src/Hooks/DataFetch.js
@@ -1,20 +1,45 @@
 import React, {useState, useEffect} from "react";
-import axios from 'axios'
 import ListaDosPokemons from "../Components/listaDosPokemons/listaDosPokemons";
 
 export default function DataFetch (){
-    const [pokemon, setPokemon] = useState([])
 
-    useEffect (() => {
-        axios.get ('https://pokeapi.co/api/v2/pokemon/?limit=251')
-        .then(res => {  
-            setPokemon(res.data.results.map(pokemon => pokemon.name))
+    const[allPokemons, setAllPokemons] = useState([])
+
+    const getAllPokemons = async () => {
+        const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
+        const data = await res.json()
+
+        function createPokemonObject(results)  {
+        results.forEach( async pokemon => {
+            const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+            const data =  await res.json()
+            setAllPokemons( currentList => [...currentList, data])
         })
+        }
+        createPokemonObject(data.results)
+    }
+
+    useEffect(() => {
+    getAllPokemons()
     }, [])
 
+
     return (
-        <div>
-            <ListaDosPokemons pokemon={pokemon} />
-        </div>
+        <>        
+            <div>
+                {allPokemons.map( (pokemonStats, index) => 
+                    <ListaDosPokemons
+                    key={index}
+                    id={pokemonStats.id}
+                    img={pokemonStats.sprites.other.dream_world.front_default}
+                    name={pokemonStats.name}
+                    hp= {pokemonStats.stats[0].base_stat}
+                    ataque= {pokemonStats.stats[1].base_stat}
+                    defesa= {pokemonStats.stats[2].base_stat}
+                    velocidade= {pokemonStats.stats[5].base_stat}
+                    typos={pokemonStats.types}
+                />)}
+            </div>
+        </>
     )
 }
