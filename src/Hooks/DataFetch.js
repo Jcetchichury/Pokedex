e72af16f5585b3217e6bf6921f98f776a8054da0
José Etchichury <jcetchichury@hotmail.com>
2022-08-15 18:08:29 -0300

    feat: adiona Filtro nos pokemons em nome e id

--- src/Hooks/DataFetch.js
+++ src/Hooks/DataFetch.js
@@ -1,9 +1,10 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect } from "react";
 import ListaDosPokemons from "../Components/listaDosPokemons/listaDosPokemons";
 
 
 export default function DataFetch (){
     const [pokemons, setPokemons] = useState([]);
+    const [busca, setBusca] = useState("")
 
     useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon/?limit=251')
@@ -11,9 +12,19 @@ export default function DataFetch (){
         .then(json => setPokemons(json.results))
     }, [])
 
-    return (       
+    const pokemonsFilter = pokemons.filter((pokemons) => 
+        pokemons.name.includes(busca.toLocaleLowerCase()) || pokemons.url.includes(busca)
+    )
+
+    return (        
             <div>
-                {pokemons.map( (pokemon, index) => 
+                <input
+                type="text"
+                className="busca"
+                value={busca}
+                onChange = {(ev) => setBusca(ev.target.value)}
+                ></input>
+                {pokemonsFilter.map( (pokemon, index) => 
                     <ListaDosPokemons
                         key = {index}
                         pokemonData = {pokemon.url}
