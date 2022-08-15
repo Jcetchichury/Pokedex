e390364f21eab5c07f9282fe9bfb32067b557afa
José Etchichury <jcetchichury@hotmail.com>
2022-08-15 15:21:46 -0300

    feat: adiocionado um easterEgg do pikachu

--- src/page/pokemonStatus.jsx
+++ src/page/pokemonStatus.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ListaPokemon from '../Components/listaPekemons/listaPokemon.jsx';
 import StatusPokemon from '../Components/statusPokemon/statusPokemon.jsx';
 import '../App.css';
-import { useParams } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
 
 function PokemonStatus() {
-    const { id } = useParams()
+    // const { id } = useParams()
     
     
 
@@ -13,7 +13,7 @@ function PokemonStatus() {
         <>
             <div className='body'>
                 <ListaPokemon />
-                <StatusPokemon id = {id}/>
+                <StatusPokemon />
             </div>
         </>
     );
