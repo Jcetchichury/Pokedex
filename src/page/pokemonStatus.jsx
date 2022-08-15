4146b301e60e882492e0de4af74f33b6c25ec6ca
José Etchichury <jcetchichury@hotmail.com>
2022-08-15 15:22:21 -0300

    retirado os comentarios

--- src/page/pokemonStatus.jsx
+++ src/page/pokemonStatus.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import ListaPokemon from '../Components/listaPekemons/listaPokemon.jsx';
 import StatusPokemon from '../Components/statusPokemon/statusPokemon.jsx';
 import '../App.css';
-// import { useParams } from 'react-router-dom';
 
 function PokemonStatus() {
-    // const { id } = useParams()
     
     
 
