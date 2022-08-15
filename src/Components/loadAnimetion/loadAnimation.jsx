e900befc1b0d5a20866b48f926c215188b48db41
José Etchichury <jcetchichury@hotmail.com>
2022-08-15 08:26:01 -0300

    feat: foi trocado o gif do load

--- src/Components/loadAnimetion/loadAnimation.jsx
+++ src/Components/loadAnimetion/loadAnimation.jsx
@@ -8,8 +8,10 @@ function LoadAnimation() {
         <>
         <section>
             <div className="bordaStatus">
-                <img src={'https://th.bing.com/th/id/R.ce914ed53571a4a94078c5f46b9d7f38?rik=YpGF9DtdEx9fyA&riu=http%3a%2f%2fimages5.fanpop.com%2fimage%2fphotos%2f31500000%2fGifs-pokemon-31559301-500-342.gif&ehk=bo5G4TxyJitsllYLhRGQQtH1nmjUlDIGi1q8QoiV%2bPU%3d&risl=&pid=ImgRaw&r=0'} alt='animation' className="imag" />
-                
+                <img 
+                src={'https://art.pixilart.com/ecd486929fa4600.gif'} 
+                alt='animation' className="imag" 
+                />
 
                 <div className="tipos">
                     <Type />
